74925b34a5350bdb09c1ceec993742c92ade8f3a
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2020-03-09 07:51:00 -0300

    added control for private routes

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import RegisterScreen from './screens/auth/register'
 import LoginScreen from './screens/auth/login'
 import NotesIndexScreen from './screens/notes/index'
 import UserEditScreen from './screens/users/edit'
+import PrivateRoute from './components/auth/PrivateRouter'
 
 const Routes = () => {
   return (
@@ -14,8 +15,8 @@ const Routes = () => {
         <Route exact path='/' component={HomeScreen} />
         <Route exact path='/register' component={RegisterScreen} />
         <Route exact path='/login' component={LoginScreen} />
-        <Route exact path='/notes' component={NotesIndexScreen} />
-        <Route exact path='/users/edit' component={UserEditScreen} />
+        <PrivateRoute exact path='/notes' component={NotesIndexScreen} />
+        <PrivateRoute exact path='/users/edit' component={UserEditScreen} />
       </Switch>
     </BrowserRouter>
   )
